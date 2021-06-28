56d2fc42de22c1db2aad9ee3f89af2a1e7b75058
pforjoeUPS <55798291+pforjoeUPS@users.noreply.github.com>
2021-06-28 16:15:19 -0400

    added return stats and hedge metrics analytics (#2) (#3)
    Co-authored-by: Maddie C <mchoi@ups.com>
    Co-authored-by: Zach W <zwells@ups.com>

--- EquityHedging/scripts/script.py
+++ EquityHedging/scripts/script.py
@@ -18,13 +18,14 @@ include_fi = False
 weighted = [True, False]
 strat_drop_list = ['99%/90% Put Spread', 'Vortex']
 new_strat = False
-returns_ups = dm.get_equity_hedge_returns(equity_bmk, include_fi, strat_drop_list, False)
+returns_ups = dm.get_equity_hedge_returns(equity_bmk, include_fi, strat_drop_list)
 
 #Add new strat
-strategy_list = ['FVA', 'Def Term Premia', 'FVA + Def Term Premia']
+strategy_list = ['FVA', 'Def Term Premia', 'FVA+Def Term Premia']
 filename = 'Credit Suisse_SX5E_Tail_FSFVA_20210528_UPS Pension.xlsx'
 sheet_name = 'data'
 new_strategy = dm.get_new_strategy_returns_data(filename, sheet_name, strategy_list)
+new_strategy=new_strategy[['Def Term Premia']]
 new_strategy_dict = dm.get_data_dict(new_strategy, data_type='index')
 returns = dm.merge_dicts(returns_ups, new_strategy_dict)
 #returns['Weekly'] = returns['Weekly'][:-1]
@@ -56,6 +57,6 @@ quintile_df = summary.get_quintile_data(returns, notional_weights,weighted=True)
 annual_dollar_returns = summary.get_annual_dollar_returns(returns, notional_weights)
 
 #run report
-equity_hedge_report = 'equity_hedge_analysis_test'
-selloffs = False
+equity_hedge_report = 'equity_hedge_analysis_test_cs_Def Term Premia'
+selloffs = True
 rp.get_equity_hedge_report(equity_hedge_report, returns, notional_weights, include_fi, new_strat, weighted[0], selloffs)
