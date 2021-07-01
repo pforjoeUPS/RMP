023df9c425777598a6e7d53ee19354f9f6c7bd4f
Maddie C <mchoi@ups.com>
2021-07-01 10:44:04 -0400

    Format Return Stats and Hedge Metrics in Jupyter Notebook

--- EquityHedging/scripts/script.py
+++ EquityHedging/scripts/script.py
@@ -21,11 +21,10 @@ new_strat = False
 returns_ups = dm.get_equity_hedge_returns(equity_bmk, include_fi, strat_drop_list)
 
 #Add new strat
-strategy_list = ['FVA', 'Def Term Premia', 'FVA+Def Term Premia']
-filename = 'Credit Suisse_SX5E_Tail_FSFVA_20210528_UPS Pension.xlsx'
+strategy_list = ['test']
+filename = '2021.06.08 - Short 1W Var+Pulse Combination Tracks.xlsx'
 sheet_name = 'data'
 new_strategy = dm.get_new_strategy_returns_data(filename, sheet_name, strategy_list)
-new_strategy=new_strategy[['Def Term Premia']]
 new_strategy_dict = dm.get_data_dict(new_strategy, data_type='index')
 returns = dm.merge_dicts(returns_ups, new_strategy_dict)
 #returns['Weekly'] = returns['Weekly'][:-1]
@@ -57,6 +56,6 @@ quintile_df = summary.get_quintile_data(returns, notional_weights,weighted=True)
 annual_dollar_returns = summary.get_annual_dollar_returns(returns, notional_weights)
 
 #run report
-equity_hedge_report = 'equity_hedge_analysis_test_cs_Def Term Premia'
+equity_hedge_report = 'check'
 selloffs = True
 rp.get_equity_hedge_report(equity_hedge_report, returns, notional_weights, include_fi, new_strat, weighted[0], selloffs)
