70fb2a414485c6503d3cb657dc88b29fea6d0657
Maddie C <mchoi@ups.com>
2021-07-16 16:37:20 -0400

    moved get_symbols function to plots.py

--- EquityHedging/reporting/plots.py
+++ EquityHedging/reporting/plots.py
@@ -10,6 +10,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from heatmap import corrplot
 from ..analytics.corr_stats import get_corr_analysis
+import pandas as pd
+
 
 sns.set(color_codes=True, font_scale=1.2)
 
@@ -88,3 +90,46 @@ def plot_corr(df_returns, notional_weights=[], include_fi=False):
     for key,value in corr_dict.items():
         plot_heatmap(value[0], key, value[1], cmap='coolwarm')
    
+
+def get_symbols(df_normal, weighted_hedge = True):
+    '''
+    Obtains a list of symbols and index's the corresponding amount of strategies
+
+    Parameters
+    ----------
+    df_normal : data frame
+        data frame 
+    weighted_hedge : boolean
+        is weighted hedge included in your data
+
+    Returns
+    -------
+    symbols : data frame
+        data frame with strategy names assigned to a number that corresponds to a symbol
+
+    '''
+    #get length of columns
+    n=len(df_normal.columns)
+    
+    #get column names of df_normal
+    c=list(df_normal.columns)
+    
+    #creates a list of the corresponding number of the symbols we want
+    a=list(range(0,33))
+    b=list(range(46,52))
+    for i in b: 
+        a.append(i)
+        
+    if weighted_hedge == True:
+        symbol_list= a[0:n-1]
+        #weighted hedge wil always be symbol 236
+        symbol_list.append(224)
+    else:
+        symbol_list= a[0:n]
+
+    #Creates a data frame that assigns each strategy with a symbol
+    symbols=pd.DataFrame(columns=c,index=[0])
+    
+    symbols.iloc[0] = symbol_list
+    
+    return symbols
