5689ea88705c397d42c95d1300c6a726ae6c9696
pforjoe <pforjoe@gmail.com>
2021-07-20 14:12:47 -0400

    Create def_universe.py

---
+++ EquityHedging/scripts/def_universe.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 20 14:11:19 2021
+
+@author: Powis Forjoe
+"""
+
+from EquityHedging.datamanager import bbg_manager as bbg
+from EquityHedging.datamanager import data_manager as dm
+from EquityHedging.reporting.excel import reports as rp
+def_uni = bbg.get_price_data(['TLT Equity', 'CCRV US Equity', 'IEF US Equity','UX1 Index','XAU Curncy'], '2007-12-28','2021-03-31')
+def_uni = dm.get_data_dict(def_uni)
+rp.get_returns_report('def_uni',def_uni)
