d3428428d46556dc7cbdaad5e58e90e2cd3e6d79
pforjoe <pforjoe@gmail.com>
2021-07-20 11:59:58 -0400

    Update add_data_temp.py

--- EquityHedging/scripts/add_data_temp.py
+++ EquityHedging/scripts/add_data_temp.py
@@ -8,8 +8,11 @@ import pandas as pd
 from EquityHedging.datamanager import data_manager as dm
 from EquityHedging.reporting.excel import reports as rp
 
+ADD_BACK = .0025
 #TODO: need to automate this
 #TODO: incorporate bloomberg api code as well
+
+#Import data from bloomberg into dataframe and create dictionary with different frequencies
 new_data = pd.read_excel(dm.UPDATE_DATA +'ups_data.xlsx', sheet_name='data', index_col=0)
 new_data.columns
 new_data.columns = ['SPTR', 'SX5T','M1WD', 'Long Corp', 'STRIPS', 'Down Var',
@@ -18,39 +21,53 @@ new_data.columns = ['SPTR', 'SX5T','M1WD', 'Long Corp', 'STRIPS', 'Down Var',
 
 new_data_dict = dm.get_data_dict(new_data)
 
-vrr = pd.read_excel(dm.UPDATE_DATA+'SGI VRR USD Official Chained Tracks.xlsx', sheet_name='data', index_col=0)
+#get vrr data
+vrr = pd.read_excel(dm.UPDATE_DATA+'vrr_tracks_data.xlsx', sheet_name='data', index_col=0)
 vrr.columns =['VRR']
 vrr_dict = dm.get_data_dict(vrr)
 
-put_spread = pd.read_excel(dm.UPDATE_DATA+'Put Spread - Return Series.xlsx',
+#add back 25 bps
+for key in vrr_dict:
+    freq = dm.switch_string_freq(key)
+    vrr_dict[key]['VRR'] += ADD_BACK/(dm.switch_freq_int(freq))
+
+#get put spread data
+put_spread = pd.read_excel(dm.UPDATE_DATA+'put_spread_data.xlsx',
                              sheet_name='Daily', index_col=0)
 put_spread.columns = ['99 Rep', 'Short Put', '99%/90% Put Spread']
 put_spread = put_spread.iloc[1:,]
-put_spread.drop(['99 Rep', 'Short Put'], axis=1, inplace=True)
+put_spread = put_spread[['99%/90% Put Spread']]
+# put_spread.drop(['99 Rep', 'Short Put'], axis=1, inplace=True)
 
 put_spread_price = dm.get_prices_df(put_spread)
 put_spread_dict = dm.get_data_dict(put_spread_price)
 
+#merge vrr and put spread dicts to the new_data dict
 new_data_dict = dm.merge_dicts(new_data_dict, put_spread_dict)
 new_data_dict = dm.merge_dicts(new_data_dict, vrr_dict)
 
+#get data from returns_data.xlsx into dataframe
 returns_dict = dm.get_equity_hedge_returns(all_data=True)
-# returns_dict = {}
-# for key in new_data_dict:
-#     returns_dict[key] = pd.read_excel(dm.RETURNS_DATA_FP+'ups_equity_hedge//returns_data_1.xlsx',sheet_name=key, index_col=0)
 
+#set columns in new_data_dict to be in the same order as returns_dict
 for key in new_data_dict:
     new_data_dict[key] = new_data_dict[key][list(returns_dict[key].columns)]
-    add_back = .0025
-    freq = dm.switch_string_freq(key)
-    new_data_dict[key]['VRR'] += add_back/(dm.switch_freq_int(freq))
+    
+#clean up new_data_dict before adding to returns    
 
-new_data_dict['Daily'] = new_data_dict['Daily'].iloc[10:,]
+#remove first 3 rows from daily dataframe
+new_data_dict['Daily'] = new_data_dict['Daily'].iloc[3:,]
+
+#remove yearly dataframe from dict
 new_data_dict.pop('Yearly')
+
+#remove last row from weekly dataframe
 new_data_dict['Weekly'] = new_data_dict['Weekly'][:-1]
 
+#update returns_dict with new_data
 for key in new_data_dict:
     returns_dict[key] = returns_dict[key].append(new_data_dict[key])
 
+#create new returns report
 rp.get_returns_report('returns_data_new', returns_dict)
 
