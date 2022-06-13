18d5414f36a0db1d0b4f2aca4561cd7679d13770
samspark14 <sam.s.park14@gmail.com>
2022-06-13 15:24:33 -0400

    Create exercise_1_sam.py

---
+++ Exercises/exercise_1/exercise_1_sam.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 13 15:18:43 2022
+
+@author: Sam Park
+"""
+#Python Exercise 1
+# 1. import bmks and strats sheets into separate dataframes (bmks, strats)
+# 2. convert the bmks and strats to daily, weekly, monhtly, quarterly and yearly returns:
+	#bmks_d, bmks_w, bmks_m, bmks_q, bmks_y
+	#strats_d, strats_w, strats_m, strats_q, strats_y 
+#3. create dictionaries from bmks and strats to have daily, weekly, monhtly, quarterly and yearly data
+	#bmks_dict, strats_dict
+#4. merge bmks and strats into 1 dataframe called data_df:
+	#make bmks the first dataframe
+#5 convert data_df to daily, monthly and weekly returns:
+	#data_df_d, data_df_w, data_df_m
+#6. create a dictionary from data_df called data_dict_1
+#7. merge bmks_dict and strats_dict to data_dict_2
+
+
+import pandas as pd
