1b6df993d9b8d5252c581d584a93e741ff44ecc8
samspark14 <sam.s.park14@gmail.com>
2022-06-14 14:46:59 -0400

    Update exercise_1_sam.py

--- Exercises/exercise_1/exercise_1_sam.py
+++ Exercises/exercise_1/exercise_1_sam.py
@@ -4,19 +4,43 @@ Created on Mon Jun 13 15:18:43 2022
 
 @author: Sam Park
 """
+
 #Python Exercise 1
-# 1. import bmks and strats sheets into separate dataframes (bmks, strats)
-# 2. convert the bmks and strats to daily, weekly, monhtly, quarterly and yearly returns:
-	#bmks_d, bmks_w, bmks_m, bmks_q, bmks_y
-	#strats_d, strats_w, strats_m, strats_q, strats_y 
-#3. create dictionaries from bmks and strats to have daily, weekly, monhtly, quarterly and yearly data
-	#bmks_dict, strats_dict
-#4. merge bmks and strats into 1 dataframe called data_df:
-	#make bmks the first dataframe
-#5 convert data_df to daily, monthly and weekly returns:
-	#data_df_d, data_df_w, data_df_m
-#6. create a dictionary from data_df called data_dict_1
-#7. merge bmks_dict and strats_dict to data_dict_2
-
-
+# 1
 import pandas as pd
+bmks_df = pd.read_excel('C:\\Users\LGQ0NLN\Documents\GitHub\RMP\Exercises\exercise_1\data_for_exercise_1.xlsx',sheet_name='bmks',index_col=0)
+strats_df = pd.read_excel('C:\\Users\LGQ0NLN\Documents\GitHub\RMP\Exercises\exercise_1\data_for_exercise_1.xlsx',sheet_name='strats',index_col=0)
+
+# 2
+from EquityHedging.datamanager import data_manager as dm
+bmks_d = dm.format_data(bmks_df,freq='1D')
+bmks_w = dm.format_data(bmks_df,freq='1W')
+bmks_m = dm.format_data(bmks_df,freq='1M')
+bmks_q = dm.format_data(bmks_df,freq='1Q')
+bmks_y = dm.format_data(bmks_df,freq='1Y')
+
+strats_d = dm.format_data(strats_df,freq='1D')
+strats_w = dm.format_data(strats_df,freq='1W')
+strats_m = dm.format_data(strats_df,freq='1M')
+strats_q = dm.format_data(strats_df,freq='1Q')
+strats_y = dm.format_data(strats_df,freq='1Y')
+
+#3
+bmks_dict = dm.get_data_dict(bmks_df, data_type='index')
+strats_dict = dm.get_data_dict(strats_df, data_type='index')
+
+#4
+data_df = dm.merge_data_frames(bmks_df, strats_df)
+
+#5
+data_df_d = dm.format_data(data_df,freq='1D')
+data_df_w = dm.format_data(data_df,freq='1W')
+data_df_m = dm.format_data(data_df,freq='1M')
+
+#6
+data_dict_1 = dm.get_data_dict(data_df, data_type='index')
+
+#7
+data_dict_2 = dm.merge_dicts(bmks_dict, strats_dict)
+
+
