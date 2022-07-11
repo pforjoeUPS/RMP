26e52ca7e61d7f31d80c836160953f94e9734e2f
mchoii <maddiechoi839@gmail.com>
2022-07-11 12:51:09 -0400

    include VaR and CVaR in equity hedge report

--- EquityHedging/scripts/script.py
+++ EquityHedging/scripts/script.py
@@ -40,6 +40,7 @@ if new_strat:
 notional_weights = dm.get_notional_weights(returns['Monthly'])
 returns = dm.get_returns_VRR_Portfolio(returns, notional_weights)
 notional_weights[4:5] = [notional_weights[4] + notional_weights[5]]
+
 df_weights = get_df_weights(notional_weights, list(returns['Monthly'].columns), include_fi)
 
 
