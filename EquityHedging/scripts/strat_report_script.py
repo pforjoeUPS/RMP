6b3afe9e0156642124e4fdea6c190d83985808d8
mchoii <maddiechoi839@gmail.com>
2022-07-27 13:40:56 -0400

    Update strat report script

--- EquityHedging/scripts/strat_report_script.py
+++ EquityHedging/scripts/strat_report_script.py
@@ -16,19 +16,18 @@ strat_drop_list = ['99%/90% Put Spread','Down Var', 'Vortex', 'VOLA 3', 'Dynamic
        'GW Dispersion', 'Corr Hedge', 'Def Var']
 include_fi = False
 #create returns data dictionary for equity benchmark
-returns= dm.get_equity_hedge_returns(equity_bmk, include_fi, strat_drop_list)
-returns = returns['Monthly']
+returns= dm.get_equity_hedge_returns(equity_bmk, include_fi, strat_drop_list, only_equity=True)#
 #returns['Daily'] = returns['Daily'].iloc[list(range(13,len(returns['Daily']))),:]
 
-mix_spx = pd.read_excel(dm.NEW_DATA+'Historical Monthly Data Series v2 6_30_22.xlsx',
-                                           sheet_name = 'Sheet1', index_col=0)
-
-data_dict ={'Monthly': dm.merge_data_frames(returns, mix_spx)}
+qsp = pd.read_excel(dm.NEW_DATA+'QSP DHS Time Series.xlsx',
+                                           sheet_name = 'data', index_col=0)
+qsp_dict = dm.get_data_dict(qsp)
+#data_dict ={'Monthly': dm.merge_data_frames(returns, mix_spx)}
 #create returns data dictionary for strategy
 
 #merge dictionaries
-#full_data_dict = dm.merge_dicts(returns,full_data_dict, fillzeros=True)
+full_data_dict = dm.merge_dicts(returns,qsp_dict, fillzeros=False)
 
-strat_report = 'atl_capital_v2_6_30_22_analysis'
+strat_report = 'qsp_dhs_analysis'
 selloffs = True
-rp.generate_strat_report(strat_report, data_dict, selloffs = False)
+rp.generate_strat_report(strat_report, full_data_dict, selloffs = True)
