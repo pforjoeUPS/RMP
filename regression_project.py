f6339933b0295e07959d3c2e941553d2add37192
justinwaters03 <119724152+justinwaters03@users.noreply.github.com>
2023-06-14 11:48:27 -0400

    Update regression_project.py

--- regression_project.py
+++ regression_project.py
@@ -1,65 +1,79 @@
 from EquityHedging.datamanager import data_manager as dm
-
-#for regressions
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from matplotlib.ticker import PercentFormatter
 
 #import returns data
 equity_bmk = 'SPTR'
-strat_drop_list = []
-returns= dm.get_equity_hedge_returns(equity_bmk)
+returns = dm.get_equity_hedge_returns(equity_bmk)
 
 comparison_strategy = 'Down Var'
-#write in greater or less
-comparison_direction = 'less'
 frequency = 'Daily'
 
 #load the returns data for SPTR and the selected strategy
 sptr_returns = returns[frequency][equity_bmk]
 comparison_returns = returns[frequency][comparison_strategy]
 
-#create a DataFrame for the regression analysis
+#creates a dataframe of the isolated returns
 data = pd.concat([sptr_returns, comparison_returns], axis=1)
 data.columns = [equity_bmk, comparison_strategy]
 
-#filter the data based on the selected direction
-if comparison_direction == 'greater':
-    data = data[data[equity_bmk] >= 0]
-else:
-    data = data[data[equity_bmk] < 0]
+#splits the data accordingly
+data_sptr_pos = data[data[equity_bmk] >= 0]
+data_sptr_neg = data[data[equity_bmk] < 0]
 
-#prepare the input features (SPTR) and target variable (selected strategy)
-X = data[equity_bmk].values.reshape(-1, 1)
-y = data[comparison_strategy].values
+#prepares the input features (SPTR) and target variable (selected strategy) for each subset
+X_pos = data_sptr_pos[equity_bmk].values.reshape(-1, 1)
+y_pos = data_sptr_pos[comparison_strategy].values
+X_neg = data_sptr_neg[equity_bmk].values.reshape(-1, 1)
+y_neg = data_sptr_neg[comparison_strategy].values
+X_all = data[equity_bmk].values.reshape(-1, 1)
+y_all = data[comparison_strategy].values
 
-#fit the linear regression model
-regression_model = LinearRegression()
-regression_model.fit(X, y)
+#fits separate linear regressions
+regression_model_pos = LinearRegression()
+regression_model_pos.fit(X_pos, y_pos)
+regression_model_neg = LinearRegression()
+regression_model_neg.fit(X_neg, y_neg)
+regression_model_all = LinearRegression()
+regression_model_all.fit(X_all, y_all)
 
 #get the regression line coordinates
-x_range = np.linspace(min(X), max(X), 100)
-y_pred = regression_model.predict(x_range.reshape(-1, 1))
+x_range_pos = np.linspace(min(X_pos), max(X_pos), 100)
+y_pred_pos = regression_model_pos.predict(x_range_pos.reshape(-1, 1))
+x_range_neg = np.linspace(min(X_neg), max(X_neg), 100)
+y_pred_neg = regression_model_neg.predict(x_range_neg.reshape(-1, 1))
 
-#print the regression coefficients
-intercept = regression_model.intercept_
-coefficient = regression_model.coef_[0]
-print(f"Regression equation: {comparison_strategy} = {coefficient:.4f} * {equity_bmk} + {intercept:.4f}")
+#prints seperate linear equations 
+intercept_pos = regression_model_pos.intercept_
+coefficient_pos = regression_model_pos.coef_[0]
+intercept_neg = regression_model_neg.intercept_
+coefficient_neg = regression_model_neg.coef_[0]
+intercept_all = regression_model_all.intercept_
+coefficient_all = regression_model_all.coef_[0]
+print(f"Regression equation (SPTR >= 0): {comparison_strategy} = {coefficient_pos:.4f} * {equity_bmk} + {intercept_pos:.4f}")
+print(f"Regression equation (SPTR < 0): {comparison_strategy} = {coefficient_neg:.4f} * {equity_bmk} + {intercept_neg:.4f}")
+print(f"Regression equation (All Data): {comparison_strategy} = {coefficient_all:.4f} * {equity_bmk} + {intercept_all:.4f}")
 
-#calculate beta as the coefficient of SPTR
-beta = coefficient
-print(f"Beta: {beta:.4f}")
+#prints seperate bets for each condition
+beta_pos = coefficient_pos
+beta_neg = coefficient_neg
+beta_all = coefficient_all
+print(f"Beta (All Data): {beta_all:.4f}")
+print(f"Beta (SPTR >= 0): {beta_pos:.4f}")
+print(f"Beta (SPTR < 0): {beta_neg:.4f}")
 
-#TODO : Great job on the plot. However, the chart shoud be a scatter plot of all returns
-# with 2 different regression lines on it 
-#one regression line fitting only returns when sptr<0 and another regression line when sptr>=0
-# refer to the screen shot in the email for the proj descrription :)
-#plot the scatter plot of the data points and the regression line
-plt.scatter(X, y, color='b', label='Data Points')
-plt.plot(x_range, y_pred, color='r', label='Regression Line')
+#creates graph of points, line of best fit, etc.
+plt.scatter(X_pos, y_pos, color='g', label='Data Points (SPTR >= 0)')
+plt.scatter(X_neg, y_neg, color='b', label='Data Points (SPTR < 0)')
+plt.plot(x_range_pos, y_pred_pos, color='r', label='Regression Line (SPTR >= 0)')
+plt.plot(x_range_neg, y_pred_neg, color='orange', label='Regression Line (SPTR < 0)')
 plt.xlabel(equity_bmk)
 plt.ylabel(comparison_strategy)
 plt.title(f'Regression Analysis: {equity_bmk} vs {comparison_strategy} ({frequency} Returns)')
-plt.legend()
-plt.show()
+#change axis labels into percentages
+plt.gca().yaxis.set_major_formatter(PercentFormatter(xmax=1, decimals=1))
+plt.gca().xaxis.set_major_formatter(PercentFormatter(xmax=1, decimals=1))
+plt.plot
