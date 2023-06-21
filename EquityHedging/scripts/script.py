ee834b88bc74c62a7ec789697f3012b742ea4ac9
samspark14 <106697943+samspark14@users.noreply.github.com>
2023-06-21 10:25:09 -0400

    Updating Notional Weights

--- EquityHedging/scripts/script.py
+++ EquityHedging/scripts/script.py
@@ -38,7 +38,7 @@ if new_strat:
 
 #get notional weights
 #notional_weights = dm.get_notional_weights(returns['Monthly'])
-notional_weights = [13.5, 0.86, 1.12, 1.01, 0.77, 0.28, 1, .23, 0.98]
+notional_weights = [13.5, 0.85, 1.09, 1.00, 0.8, 0.26, 1, .23, 1.00]
 returns = dm.get_returns_VRR_Portfolio(returns, notional_weights)
 
 notional_weights[4:6] = [notional_weights[4] + notional_weights[5]]
