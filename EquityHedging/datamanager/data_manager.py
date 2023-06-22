8ac2f9fa0d3fd3276037d012753d4570ef65a38b
mchoii <maddiechoi839@gmail.com>
2023-06-22 08:58:40 -0400

    include vix and ux3 in returns data

--- EquityHedging/datamanager/data_manager.py
+++ EquityHedging/datamanager/data_manager.py
@@ -21,7 +21,7 @@ UPDATE_DATA = RETURNS_DATA_FP + 'update_strats\\'
 EQUITY_HEDGE_DATA = RETURNS_DATA_FP + 'ups_equity_hedge\\'
 QIS_UNIVERSE = CWD + '\\Cluster Analysis\\data\\'
 
-NEW_DATA_COL_LIST = ['SPTR', 'SX5T','M1WD', 'Long Corp', 'STRIPS', 'Down Var',
+NEW_DATA_COL_LIST = ['SPTR', 'SX5T','M1WD','VIX','UX3', 'Long Corp', 'STRIPS', 'Down Var',
  'Vortex', 'VOLA I', 'VOLA II','Dynamic VOLA','Dynamic Put Spread',
                     'GW Dispersion', 'Corr Hedge','Def Var (Mon)', 'Def Var (Fri)', 'Def Var (Wed)', 
                     'Commodity Basket']
@@ -61,7 +61,7 @@ def merge_data_frames(df_main, df_new,fillzeros=False):
     dataframe
     """
     
-    df = pd.merge(df_main, df_new, left_index=True, right_index=True, how='outer')
+    df = pd.merge(df_main, df_new, left_index=True, right_index=True, how='left')
     if fillzeros:
         df = df.fillna(0)
     else:
