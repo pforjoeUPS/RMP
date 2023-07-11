df8cfb7e91c86f95ca7dd264df43f04f7013c53f
Devang0903 <devangajmera09@gmail.com>
2023-07-11 11:32:44 -0400

    Updated qis update script and excel

--- Cluster Analysis/scripts/qis_newmerge_script.py
+++ Cluster Analysis/scripts/qis_newmerge_script.py
@@ -16,13 +16,12 @@ CWD = os.getcwd()
 
 # Replace with the path to your original Excel file
 existing_file = 'QIS Universe Time Series.xlsx'
-existing_sheet_name = 'CS'
+existing_sheet_name = 'Barclays'
 
 # Replace  with the path to your new Excel file
 new_file = 'update_QIS_uni.xlsx'
 new_sheet_name = 'Sheet1'
 
-#a = dm.get_qis_uni_dict()
 # Read the original Excel file into a Pandas DataFrame
 existing_df = pd.read_excel(existing_file, sheet_name=existing_sheet_name, skiprows=1, engine='openpyxl')
 
@@ -49,38 +48,41 @@ print("Number of tickers in the new data that match existing tickers:", common_t
 #Create a dictionary to store DataFrames for each ticker in the new data
 new_strategy_data_frames = {}
 
-# Iterate through the new ticker names and create separate DataFrames for each ticker
-for ticker in ticker_names_new:
-    # Extract date and price columns for the current ticker in the new data
-    date_column = new_df.iloc[2:, 0]
-    price_column = new_df[ticker][2:]
-    
-    # Create a new DataFrame for the current ticker in the new data
-    ticker_df = pd.DataFrame({'Date': date_column, ticker: price_column})
-    
-    # Convert the 'Date' column to datetime type
-    ticker_df['Date'] = pd.to_datetime(ticker_df['Date'])
-    
-    # Add the DataFrame to the dictionary with ticker as the key
-    new_strategy_data_frames[ticker] = ticker_df
 
-# Merge all DataFrames on the 'Date' column to get the final DataFrame for new data
-merged_df_new = reduce(lambda left, right: pd.merge(left, right, on='Date', how='outer'), new_strategy_data_frames.values())
-
-# Combine the existing DataFrame with the new merged DataFrame
+# Combine the existing DataFrame with the new DataFrames
 merged_df_combined = existing_df.copy()
 
-# Iterate through the new ticker names and update prices if the ticker already exists, otherwise add the new column
-for ticker in ticker_names_new:
-    if ticker in ticker_names_existing:
-        merged_df_combined[ticker] = merged_df_new[ticker]
-    else:
-        merged_df_combined = pd.merge(merged_df_combined, merged_df_new[['Date', ticker]], on='Date', how='left')
+ticker_data = {}
 
+# Create a dictionary of DataFrames for each adjacent column pair in the new data
+for i in range(0, new_df.shape[1], 2):
+    date_column = new_df.iloc[1:, i].values.flatten()
+    price_column = new_df.iloc[1:, i+1].values.flatten()
+
+    # Create a DataFrame for the current adjacent column pair
+    ticker_name = new_df.columns[i+1]
+    ticker_df = pd.DataFrame({ticker_name: price_column}, index=date_column)
+
+    # Convert the index to datetime type
+    ticker_df.index = pd.to_datetime(ticker_df.index)
+    ticker_df.index.name = 'Date'
+
+    # Add the DataFrame to the dictionary
+    ticker_data[ticker_name] = ticker_df
+
+# Iterate through the DataFrames in the ticker_data dictionary
+for ticker, ticker_df in ticker_data.items():
+    if ticker in ticker_names_existing:
+        # Replace existing ticker prices based on corresponding dates
+        merged_df_combined[ticker] = merged_df_combined.apply(lambda row: ticker_df.loc[row['Date'], ticker] if row['Date'] in ticker_df.index else "", axis=1)
+      
+    else:
+        # Left merge with existing DataFrame based on date
+        merged_df_combined = pd.merge(merged_df_combined, ticker_df, left_on='Date', right_index=True, how='left')
+        #merged_df_combined[ticker].fillna(value="NA", inplace=True)
 
 # Fill the blanks with NaN
-merged_df_combined = merged_df_combined.fillna(pd.NA)
-# Open the existing Excel file with openpyxl
+
 book = load_workbook(existing_file)
 
 # Select the existing sheet
@@ -100,7 +102,3 @@ for r_idx, row in enumerate(rows, start_row):
 # Save the updated Excel file
 book.save(existing_file)
 print("Data added to the existing sheet successfully!")
-# Save the combined DataFrame to a new Excel file
-#output_file = 'combined_strategies.xlsx'
-#merged_df_combined.to_excel(existing_file, existing_sheet_name , index=False, startrow = 1)
-
