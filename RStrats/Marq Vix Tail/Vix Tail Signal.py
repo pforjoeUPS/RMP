6006c47b5bc923d2098283f8972b5ccfe895015e
samspark14 <106697943+samspark14@users.noreply.github.com>
2023-07-12 14:38:06 -0400

    Update Vix Tail Signal.py
    Computing Combined signal now
    (Code still messy)

--- RStrats/Marq Vix Tail/Vix Tail Signal.py
+++ RStrats/Marq Vix Tail/Vix Tail Signal.py
@@ -6,124 +6,234 @@ Created on Wed Jul  5 09:16:03 2023
 """
 
 import pandas as pd
-import numpy as np
 import math as ma
-from datetime import datetime as dt
-from math import prod
-from EquityHedging.analytics import summary 
+from EquityHedging.datamanager import data_manager as dm
 
 
 #vix futures calendar
 futures_calendar = pd.read_excel('C:\\Users\\PCR7FJW\\Desktop\\Construct Vix Strategies.xlsx', sheet_name='vix_futures_calendar',names=['Dates'])
 futures_calendar_dates = list(futures_calendar['Dates'])
-underlying_data=pd.read_excel('C:\\Users\\PCR7FJW\\Desktop\\Construct Vix Strategies.xlsx', sheet_name='Inverted Curve z-score', usecols=['Date','VIX','Intraday VIX'])
+underlier_lambda = ma.exp(-2/27)
+z_score_lambda = ma.exp(-2/364)
 
 
 #reading in data
 dataF = pd.read_excel('C:\\Users\\PCR7FJW\\Desktop\\Construct Vix Strategies.xlsx', sheet_name='Replicating Portfolio')
 dataF = dataF.drop(columns=['dr', 'dt','1M Replicating', '2M Replicating', '3M Replicating', '4M Replicating'])
-dataF.rename({"Unnamed: 20":"a"}, axis="columns", inplace=True)
-dataF.drop(['a'], axis=1, inplace=True)
+dataF.drop(columns=['Unnamed: 20'], inplace=True)
 dataI = pd.read_excel('C:\\Users\\PCR7FJW\\Desktop\\Construct Vix Strategies.xlsx', sheet_name='Intraday Replicating Portfolio')
 dataI = dataI.drop(columns=['dr', 'dt','1M Replicating', '2M Replicating', '3M Replicating', '4M Replicating'])
-dataI.rename({"Unnamed: 20":"a"}, axis="columns", inplace=True)
-dataI.drop(['a'], axis=1, inplace=True)
+dataI.drop(columns=['Unnamed: 20'], inplace=True)
 
 
-#replicate futures/portfolio
+# replicate futures/portfolio
 def replicate_portfolio(data):
-    timestamps=[]
-    OneMRep=[]
+    timestamps = data['Date'].tolist()
+    OneMRep = []
     TwoMRep = []
     ThreeMRep = []
     FourMRep = []
-    for index, row in data.iterrows():
-        timestamps.append(row['Date'])
+    
+    for _, row in data.iterrows():
         x = futures_calendar_dates.index(row['Front Contract Exp']) - futures_calendar_dates.index(row['Date'])
         y = futures_calendar_dates.index(row['Front Contract Exp']) - futures_calendar_dates.index(row['Prev Contract Exp'])
-        z = x/y
+        z = x / y
+        
         if x == y:
             OneMRep.append(row['Front Price'])
             TwoMRep.append(row['Second Price'])
             ThreeMRep.append(row['Third Price'])
             FourMRep.append(row['Fourth Price'])
         else:
-            OneMRep.append((z*row['Front Price'])+((1-z)*row['Second Price']))
-            TwoMRep.append((z*row['Second Price'])+((1-z)*row['Third Price']))        
-            ThreeMRep.append((z*row['Third Price'])+((1-z)*row['Fourth Price']))    
-            FourMRep.append((z*row['Fourth Price'])+((1-z)*row['Fifth Price']))    
+            OneMRep.append((z * row['Front Price']) + ((1 - z) * row['Second Price']))
+            TwoMRep.append((z * row['Second Price']) + ((1 - z) * row['Third Price']))
+            ThreeMRep.append((z * row['Third Price']) + ((1 - z) * row['Fourth Price']))
+            FourMRep.append((z * row['Fourth Price']) + ((1 - z) * row['Fifth Price']))
             
-    replicated_data = {'1M Replicating': OneMRep, '2M Replicating': TwoMRep, '3M Replicating': ThreeMRep, '4M Replicating': FourMRep}
-    Portfolio = pd.DataFrame(replicated_data, index=timestamps)
+    replicated_data = {
+        '1M Replicating': OneMRep,
+        '2M Replicating': TwoMRep,
+        '3M Replicating': ThreeMRep,
+        '4M Replicating': FourMRep
+    }
     
+    Portfolio = pd.DataFrame(replicated_data, index=timestamps)
     return Portfolio
 
 rep_port = replicate_portfolio(dataF)
 rep_intra_port = replicate_portfolio(dataI)
 
+underlying_data_inv = pd.read_excel('C:\\Users\\PCR7FJW\\Desktop\\Construct Vix Strategies.xlsx', sheet_name='Inverted Curve z-score', usecols=['Date','VIX','Intraday VIX'])
 #calculating inverted z score (ratio z)
-def calculating_ratio_z (data):
+def calculating_ratio_z(data, rep_port, rep_intra_port, z_score_lambda):
     inverted_z = []
-    timestamps=[]
-    prev_rolling_mean = 0
-    prev_debiasing_factor = 0
-    prev_rolling_vol = 0
-    
+    timestamps = []
     rolling_mean = 0
     rolling_var = 0
     debiasing_factor = 1
     rolling_vol = 0
-    
     intraday_rolling_mean = 0
     intraday_rolling_vol = 0
-    
-    z_score_lambda = ma.exp(-2/364)
-    
-    for index, row in underlying_data.iterrows():
-        
+
+    for index, row in underlying_data_inv.iterrows():
         timestamps.append(row['Date'])
         VixD1M = row['VIX'] / rep_port.loc[str(row['Date'])]['1M Replicating']
         Intraday_VixD1M = row['Intraday VIX'] / rep_intra_port.loc[str(row['Date'])]['1M Replicating']
-        
-        if prev_rolling_mean != 0:
+
+        if rolling_mean != 0:
             #for close prices
             prev_rolling_mean = rolling_mean
-            rolling_mean = prev_rolling_mean*z_score_lambda + (1-z_score_lambda)*VixD1M
-            rolling_var = z_score_lambda*((rolling_var)+(prev_rolling_mean-rolling_mean)**2)+((1-z_score_lambda)*(VixD1M-rolling_mean)**2)
+            rolling_mean = prev_rolling_mean * z_score_lambda + (1 - z_score_lambda) * VixD1M
+            rolling_var = z_score_lambda * (rolling_var + (prev_rolling_mean - rolling_mean) ** 2) + (
+                        1 - z_score_lambda) * (VixD1M - rolling_mean) ** 2
             prev_debiasing_factor = debiasing_factor
-            debiasing_factor = prev_debiasing_factor*z_score_lambda**2 + (1-z_score_lambda)**2
-            
+            debiasing_factor = prev_debiasing_factor * z_score_lambda ** 2 + (1 - z_score_lambda) ** 2
             prev_rolling_vol = rolling_vol
-            rolling_vol = ma.sqrt((rolling_var/(1-debiasing_factor)))
+            rolling_vol = ma.sqrt(rolling_var / (1 - debiasing_factor))
             
             #for snap prices
-            intraday_rolling_mean = prev_rolling_mean*z_score_lambda + (1-z_score_lambda)*Intraday_VixD1M
-            intraday_rolling_vol = ((prev_rolling_vol**2+(prev_rolling_mean-intraday_rolling_mean)**2)*z_score_lambda + (1-z_score_lambda)*(Intraday_VixD1M-intraday_rolling_mean)**2)**0.5
-                 
+            intraday_rolling_mean = prev_rolling_mean * z_score_lambda + (1 - z_score_lambda) * Intraday_VixD1M
+            intraday_rolling_vol = ma.sqrt(
+                (prev_rolling_vol ** 2 + (prev_rolling_mean - intraday_rolling_mean) ** 2) * z_score_lambda + (
+                            1 - z_score_lambda) * (Intraday_VixD1M - intraday_rolling_mean) ** 2)
+
         else:
-            prev_rolling_mean = VixD1M
-            rolling_mean = prev_rolling_mean*z_score_lambda + (1-z_score_lambda)*VixD1M
-            rolling_var = (VixD1M-rolling_mean)**2
-            
-        #calculating z-score
-        z_score = (Intraday_VixD1M-intraday_rolling_mean)/intraday_rolling_vol
-        
+            rolling_mean = VixD1M
+            rolling_var = (VixD1M - rolling_mean) ** 2
+
+        z_score = (Intraday_VixD1M - intraday_rolling_mean) / intraday_rolling_vol
         inverted_z.append(z_score)
-        data = {'inverted_z_score': inverted_z}
-        inverted_z_scores = pd.DataFrame(data, index=timestamps)
-        
+
+    inverted_z_scores = pd.DataFrame({'inverted_z_score': inverted_z}, index=timestamps)
     return inverted_z_scores
 
-VIXtoVIXFutures_z = calculating_ratio_z(underlying_data)
+VIXtoVIXFutures_z = calculating_ratio_z(underlying_data_inv, replicate_portfolio(dataF), replicate_portfolio(dataI), z_score_lambda)
 
+
+underlying_data_SPX_VIX = pd.read_excel('C:\\Users\\PCR7FJW\\Desktop\\Construct Vix Strategies.xlsx', sheet_name='Hist vs Imp Vol z score', usecols=['Date','SPX','VIX','Intraday SPX','Intraday VIX'])
 #calculating differnce between Imp(vix) and Hist(SPX) Vol z score
+def calculating_diff_z_score(data,underlier_lambda,z_score_lambda):
+    diff_z = []
+    timestamps = []
+    prev_spx = underlying_data_SPX_VIX['SPX'][0]
+    prev_iSPX = underlying_data_SPX_VIX['Intraday SPX'][0]
+    skip_first = True
+    
+    prev_ret_roll_mean = 0
+    prev_ret_roll_var = 0
+    ret_roll_mean = 0
+    ret_roll_var = 0
+    u_debiasing_factor = 1
+    prev_hist_vol = 0
+    hist_vol = 0
+    
+    diff = 0
+    prev_diff_roll_mean = 0
+    prev_diff_roll_var = 0
+    prev_diff_roll_stdev = 0
+    diff_roll_mean = 0
+    diff_roll_var = 0
+    z_debiasing_factor = 1
+    diff_roll_stdev = 0
+    
+    iret_roll_mean = 0
+    ihist_vol = 0
+    
+    idiff = 0
+    idiff_roll_mean = 0
+    idiff_roll_stdev = 0
+    
+    for index, row in underlying_data_SPX_VIX.iterrows():
+        
+        timestamps.append(row['Date'])
+        
+        if skip_first:
+            skip_first = False
+            continue
+        elif prev_ret_roll_mean == 0 and prev_spx == row['SPX']:
+            #return
+            daily_ret = (row['SPX']/prev_spx)-1
+            ret_roll_mean = daily_ret
+            ret_roll_var = (daily_ret - ret_roll_mean)**2
+            
+            prev_spx = row['SPX']
+            
+            #intraday
+            intrdaily_ret = (row['Intraday SPX']/prev_iSPX)-1
+            iret_roll_mean = intrdaily_ret
+            
+            prev_iSPX = row['Intraday SPX']
+
+            
+        else:
+            #return
+            daily_ret = (row['SPX']/prev_spx)-1
+            ret_roll_mean = underlier_lambda*prev_ret_roll_mean+(1-underlier_lambda)*daily_ret
+            ret_roll_var = underlier_lambda*(prev_ret_roll_var+(ret_roll_mean-prev_ret_roll_mean)**2)+(1-underlier_lambda)*(daily_ret-ret_roll_mean)**2
+            u_debiasing_factor = u_debiasing_factor*underlier_lambda**2 + (1-underlier_lambda)**2
+            hist_vol = ((ret_roll_var/(1-u_debiasing_factor))**0.5)*(252**0.5)*100
+            
+            #intraday
+            intrdaily_ret = (row['Intraday SPX']/prev_spx)-1
+            iret_roll_mean = underlier_lambda*prev_ret_roll_mean + (1-underlier_lambda)*intrdaily_ret
+            if hist_vol < 0:    
+                pass
+            else:
+                ihist_vol =(((prev_hist_vol/(100*252**0.5))**2 + (prev_ret_roll_mean-iret_roll_mean)**2)*underlier_lambda + (1-underlier_lambda)*(intrdaily_ret-iret_roll_mean)**2)**0.5*252**0.5*100
+            #return
+            prev_spx = row['SPX']        
+            prev_ret_roll_mean = ret_roll_mean
+            prev_ret_roll_var = ret_roll_var
+            prev_iSPX = row['Intraday SPX']
+            prev_hist_vol = hist_vol
+    
+            #diff
+            diff = hist_vol - row['VIX']
+            
+            #intraday diff
+            idiff = ihist_vol - row['Intraday VIX']
+            if prev_diff_roll_mean == 0:
+                #diff
+                diff_roll_mean = diff
+                diff_roll_var = (diff - diff_roll_mean)**2
+    
+                #intrday diff
+                idiff_roll_mean = idiff
+    
+                prev_diff_roll_mean = ret_roll_mean
+                prev_diff_roll_var = ret_roll_var
+            else:
+                #diff
+                diff_roll_mean = z_score_lambda*prev_diff_roll_mean + (1-z_score_lambda)*diff
+                diff_roll_var = z_score_lambda*(prev_diff_roll_var+(diff_roll_mean-prev_diff_roll_mean)**2) + (1-z_score_lambda)*(diff-diff_roll_mean)**2
+                z_debiasing_factor = z_debiasing_factor*z_score_lambda**2 + (1-z_score_lambda)**2
+                diff_roll_stdev = ma.sqrt(diff_roll_var/(1-z_debiasing_factor))
+                
+                #intraday diff
+                idiff_roll_mean = prev_diff_roll_mean*z_score_lambda+(1-z_score_lambda)*idiff
+                idiff_roll_stdev =((prev_diff_roll_stdev**2+(prev_diff_roll_mean-idiff_roll_mean)**2)*z_score_lambda + (1-z_score_lambda)*(idiff-idiff_roll_mean)**2)**0.5
+                
+                prev_diff_roll_mean = diff_roll_mean
+                prev_diff_roll_var = diff_roll_var
+                prev_diff_roll_stdev = diff_roll_stdev
+                
+        z_score = (idiff-idiff_roll_mean)/idiff_roll_stdev
+        diff_z.append(z_score)
+    del timestamps[:4]
+    del diff_z[:3]
+    hist_imp_z_scores = pd.DataFrame({'diff_z_score': diff_z}, index=timestamps)
+    return hist_imp_z_scores
 
 
+diff_z_score = calculating_diff_z_score(underlying_data_SPX_VIX,underlier_lambda,z_score_lambda)
 
+comb = dm.merge_data_frames(VIXtoVIXFutures_z, diff_z_score)
 
-
-
-
+combined_signal = []
+for index, row in comb.iterrows():
+    comb_z = 0.5*row['inverted_z_score'] + 0.5*row['diff_z_score']
+    combined_signal.append(comb_z)
+comb['Signal'] = combined_signal
 
         
 
