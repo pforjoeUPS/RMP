6a34abec5706ce1cc50c9d9b01cbf36785b8d30c
mchoii <maddiechoi839@gmail.com>
2023-07-12 14:59:12 -0400

    update spider chart script

--- spider chart.py
+++ spider chart.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import EquityHedging.analytics.util as ut
 import EquityHedging.analytics.hedge_metrics as hm
 from EquityHedging.datamanager import data_manager as dm
+from EquityHedging.analytics import summary 
+
 
 #import returns data
 equity_bmk = 'SPTR'
@@ -22,16 +24,21 @@ notional_weights[4:6] = [notional_weights[4] + notional_weights[5]]
 df_weights = ut.get_df_weights(notional_weights, list(returns['Monthly'].columns), include_fi)
 returns_df = returns['Daily']
 
+#get weighted strats and weighted hedges
+r = summary.get_weighted_data(returns_df, notional_weights)
+
 # Step 3: Compute hedge metrics, ask to find where to find dataframe of the returns
-df_hedge_metrics = hm.get_hedge_metrics(returns_df,freq = "1D", full_list=True)
+df_hedge_metrics = hm.get_hedge_metrics(r,freq = "1D", full_list=True)
 
 # Step 4: Select specific metrics for spider chart
-metrics_to_plot = hm.get_hedge_metrics(returns_df, freq = "1D", full_list = True)
-test_var = metrics_to_plot.loc[:, 3]
-#hedge_metrics = df_hedge_metrics.loc[:, metrics_to_plot]
+metrics_to_plot = df_hedge_metrics.transpose()
+metrics = metrics_to_plot[["Benefit Cum", "Convexity Cum", "Cost Cum", "Downside Reliability", "Decay Days (25% retrace)"]]
 
 # Step 5: Normalize the hedge metrics
-normalized_data = ut.get_normalized_data(hm.hedge_metrics)
+normalized_data = ut.get_normalized_data(metrics)
+
+test = normalized_data.transpose()
+x = test['SPTR']
 
 # Step 6: Create a spider chart for each strategy
 for strategy in normalized_data.index:
