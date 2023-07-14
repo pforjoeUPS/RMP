69f3ca8050a9c3365a2b428a0ec5848a70b56ed1
samspark14 <106697943+samspark14@users.noreply.github.com>
2023-07-14 12:05:02 -0400

    Update Vix Tail Signal.py

--- RStrats/Marq Vix Tail/Vix Tail Signal.py
+++ RStrats/Marq Vix Tail/Vix Tail Signal.py
@@ -60,8 +60,7 @@ def replicate_portfolio(data):
     Portfolio = pd.DataFrame(replicated_data, index=timestamps)
     return Portfolio
 
-rep_port = replicate_portfolio(dataF)
-rep_intra_port = replicate_portfolio(dataI)
+
 
 underlying_data_inv = pd.read_excel('C:\\Users\\PCR7FJW\\Desktop\\Construct Vix Strategies.xlsx', sheet_name='Inverted Curve z-score', usecols=['Date','VIX','Intraday VIX'])
 #calculating inverted z score (ratio z)
@@ -119,29 +118,17 @@ def calculating_diff_z_score(data,underlier_lambda,z_score_lambda):
     prev_iSPX = underlying_data_SPX_VIX['Intraday SPX'][0]
     skip_first = True
     
-    prev_ret_roll_mean = 0
-    prev_ret_roll_var = 0
-    ret_roll_mean = 0
-    ret_roll_var = 0
+    prev_ret_roll_mean = prev_ret_roll_var = prev_hist_vol = 0
+    ret_roll_mean = ret_roll_var = hist_vol = 0
     u_debiasing_factor = 1
-    prev_hist_vol = 0
-    hist_vol = 0
     
-    diff = 0
-    prev_diff_roll_mean = 0
-    prev_diff_roll_var = 0
-    prev_diff_roll_stdev = 0
-    diff_roll_mean = 0
-    diff_roll_var = 0
+    prev_diff_roll_mean = prev_diff_roll_var = prev_diff_roll_stdev = 0
+    diff = diff_roll_mean = diff_roll_var = diff_roll_stdev = 0
     z_debiasing_factor = 1
-    diff_roll_stdev = 0
     
-    iret_roll_mean = 0
-    ihist_vol = 0
+    iret_roll_mean = ihist_vol = 0
     
-    idiff = 0
-    idiff_roll_mean = 0
-    idiff_roll_stdev = 0
+    idiff = idiff_roll_mean = idiff_roll_stdev = 0
     
     for index, row in underlying_data_SPX_VIX.iterrows():
         
@@ -151,35 +138,31 @@ def calculating_diff_z_score(data,underlier_lambda,z_score_lambda):
             skip_first = False
             continue
         elif prev_ret_roll_mean == 0 and prev_spx == row['SPX']:
-            #return
-            daily_ret = (row['SPX']/prev_spx)-1
+            daily_ret = (row['SPX'] / prev_spx) - 1
             ret_roll_mean = daily_ret
-            ret_roll_var = (daily_ret - ret_roll_mean)**2
-            
+            ret_roll_var = (daily_ret - ret_roll_mean) ** 2
             prev_spx = row['SPX']
             
-            #intraday
-            intrdaily_ret = (row['Intraday SPX']/prev_iSPX)-1
+            intrdaily_ret = (row['Intraday SPX'] / prev_iSPX) - 1
             iret_roll_mean = intrdaily_ret
-            
             prev_iSPX = row['Intraday SPX']
 
-            
         else:
-            #return
-            daily_ret = (row['SPX']/prev_spx)-1
-            ret_roll_mean = underlier_lambda*prev_ret_roll_mean+(1-underlier_lambda)*daily_ret
-            ret_roll_var = underlier_lambda*(prev_ret_roll_var+(ret_roll_mean-prev_ret_roll_mean)**2)+(1-underlier_lambda)*(daily_ret-ret_roll_mean)**2
-            u_debiasing_factor = u_debiasing_factor*underlier_lambda**2 + (1-underlier_lambda)**2
-            hist_vol = ((ret_roll_var/(1-u_debiasing_factor))**0.5)*(252**0.5)*100
+            daily_ret = (row['SPX'] / prev_spx) - 1
+            ret_roll_mean = underlier_lambda * prev_ret_roll_mean + (1-underlier_lambda) * daily_ret
+            ret_roll_var = underlier_lambda * (prev_ret_roll_var + (ret_roll_mean - prev_ret_roll_mean) ** 2) \
+                            + (1 - underlier_lambda) * (daily_ret-ret_roll_mean) ** 2
+            u_debiasing_factor = u_debiasing_factor * underlier_lambda ** 2 + (1 - underlier_lambda) ** 2
+            hist_vol = ((ret_roll_var / (1 - u_debiasing_factor)) ** 0.5) * (252 ** 0.5) * 100
             
-            #intraday
-            intrdaily_ret = (row['Intraday SPX']/prev_spx)-1
-            iret_roll_mean = underlier_lambda*prev_ret_roll_mean + (1-underlier_lambda)*intrdaily_ret
+            intrdaily_ret = (row['Intraday SPX'] / prev_spx) - 1
+            iret_roll_mean = underlier_lambda * prev_ret_roll_mean + (1 - underlier_lambda) * intrdaily_ret
             if hist_vol < 0:    
                 pass
             else:
-                ihist_vol =(((prev_hist_vol/(100*252**0.5))**2 + (prev_ret_roll_mean-iret_roll_mean)**2)*underlier_lambda + (1-underlier_lambda)*(intrdaily_ret-iret_roll_mean)**2)**0.5*252**0.5*100
+                ihist_vol =(((prev_hist_vol / (100 * 252 ** 0.5)) ** 2 + (prev_ret_roll_mean-iret_roll_mean) ** 2) \
+                            * underlier_lambda + (1 - underlier_lambda) * (intrdaily_ret - iret_roll_mean) ** 2) ** \
+                            0.5 * 252 ** 0.5 * 100
             #return
             prev_spx = row['SPX']        
             prev_ret_roll_mean = ret_roll_mean
@@ -187,31 +170,28 @@ def calculating_diff_z_score(data,underlier_lambda,z_score_lambda):
             prev_iSPX = row['Intraday SPX']
             prev_hist_vol = hist_vol
     
-            #diff
             diff = hist_vol - row['VIX']
-            
-            #intraday diff
             idiff = ihist_vol - row['Intraday VIX']
             if prev_diff_roll_mean == 0:
-                #diff
                 diff_roll_mean = diff
                 diff_roll_var = (diff - diff_roll_mean)**2
-    
-                #intrday diff
+                prev_diff_roll_mean = ret_roll_mean
+                prev_diff_roll_var = ret_roll_var
+
                 idiff_roll_mean = idiff
-    
+
                 prev_diff_roll_mean = ret_roll_mean
                 prev_diff_roll_var = ret_roll_var
             else:
-                #diff
                 diff_roll_mean = z_score_lambda*prev_diff_roll_mean + (1-z_score_lambda)*diff
-                diff_roll_var = z_score_lambda*(prev_diff_roll_var+(diff_roll_mean-prev_diff_roll_mean)**2) + (1-z_score_lambda)*(diff-diff_roll_mean)**2
+                diff_roll_var = z_score_lambda*(prev_diff_roll_var+(diff_roll_mean-prev_diff_roll_mean)**2) + \
+                            (1-z_score_lambda)*(diff-diff_roll_mean)**2
                 z_debiasing_factor = z_debiasing_factor*z_score_lambda**2 + (1-z_score_lambda)**2
                 diff_roll_stdev = ma.sqrt(diff_roll_var/(1-z_debiasing_factor))
                 
-                #intraday diff
                 idiff_roll_mean = prev_diff_roll_mean*z_score_lambda+(1-z_score_lambda)*idiff
-                idiff_roll_stdev =((prev_diff_roll_stdev**2+(prev_diff_roll_mean-idiff_roll_mean)**2)*z_score_lambda + (1-z_score_lambda)*(idiff-idiff_roll_mean)**2)**0.5
+                idiff_roll_stdev =((prev_diff_roll_stdev**2+(prev_diff_roll_mean-idiff_roll_mean)**2)*z_score_lambda \
+                                   + (1-z_score_lambda)*(idiff-idiff_roll_mean)**2)**0.5
                 
                 prev_diff_roll_mean = diff_roll_mean
                 prev_diff_roll_var = diff_roll_var
@@ -227,14 +207,17 @@ def calculating_diff_z_score(data,underlier_lambda,z_score_lambda):
 
 diff_z_score = calculating_diff_z_score(underlying_data_SPX_VIX,underlier_lambda,z_score_lambda)
 
-comb = dm.merge_data_frames(VIXtoVIXFutures_z, diff_z_score)
+def calculating_combined_signals (inverted_z, diff_z):
+    comb = dm.merge_data_frames(VIXtoVIXFutures_z, diff_z_score)
+    combined_signal = []
+    for index, row in comb.iterrows():
+        comb_z = 0.5*row['inverted_z_score'] + 0.5*row['diff_z_score']
+        combined_signal.append(comb_z)
+    comb['Signal'] = combined_signal
+    return comb
 
-combined_signal = []
-for index, row in comb.iterrows():
-    comb_z = 0.5*row['inverted_z_score'] + 0.5*row['diff_z_score']
-    combined_signal.append(comb_z)
-comb['Signal'] = combined_signal
-
-        
+#Nextsteps:
+    #Make code more dynamic by beging able to do adjusted z-scores, change signal weights g_(num)
+    #Calculate cost/returns and create index
 
 
