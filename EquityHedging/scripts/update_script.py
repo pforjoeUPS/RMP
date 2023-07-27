bd728ef662813252bafd1217cf3c792d5f25cc8c
pforjoeUPS <pforjoe@ups.com>
2023-07-27 15:08:24 -0400

    Update update_script.py

--- EquityHedging/scripts/update_script.py
+++ EquityHedging/scripts/update_script.py
@@ -5,5 +5,12 @@ Created on Thu Jan 12 00:00:57 2023
 @author: NVG9HXP
 """
 
-from EquityHedging.datamanager import data_manager as dm
-from EquityHedging.reporting.excel import reports as rp
+from EquityHedging.datamanager import data_updater as du
+
+du.update_nexen_liq_alts_data()
+du.update_liq_alts_bmk_data()
+du.update_innocap_liq_alts_data()
+du.update_hf_bmk_data()
+du.update_bmk_data()
+du.update_asset_class_data()
+du.update_eq_hedge_returns()
