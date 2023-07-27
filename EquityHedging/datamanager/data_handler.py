42f57ffc72b922c7dd78f90aed743189659fce14
pforjoeUPS <pforjoe@ups.com>
2023-07-27 15:07:49 -0400

    Update data_handler.py

--- EquityHedging/datamanager/data_handler.py
+++ EquityHedging/datamanager/data_handler.py
@@ -12,16 +12,15 @@ import pandas as pd
 CWD = os.getcwd()
 RETURNS_DATA_FP = CWD +'\\EquityHedging\\data\\returns_data\\'
 BMK_DATA_FP = RETURNS_DATA_FP+'bmk_returns.xlsx'
+HF_BMK_DATA_FP = RETURNS_DATA_FP+'hf_bmks.xlsx'
 LIQ_ALTS_BMK_DATA_FP = RETURNS_DATA_FP+'liq_alts_bmks.xlsx'
-LIQ_ALTS_PORT_DATA_FP = RETURNS_DATA_FP+'liq_alts_data.xlsx'
+LIQ_ALTS_PORT_DATA_FP = RETURNS_DATA_FP+'nexen_liq_alts_data.xlsx'
 LIQ_ALTS_MGR_DICT = {'Global Macro': ['1907 Penso Class A','Bridgewater Alpha', 'DE Shaw Oculus Fund',
-                                      'Element Capital'],
+                                      'Element Capital', 'JSC Vantage'],
                      'Trend Following': ['1907 ARP TF','1907 Campbell TF', '1907 Systematica TF',
                                          'One River Trend'],
                      'Absolute Return':['1907 ARP EM',  '1907 III CV', '1907 III Class A',
-                                        # 'ABC Reversion',
-                                        'Acadian Commodity AR',
-                                        'Blueshift', 'Duality', 'Elliott']
+                                        'Acadian Commodity AR','Blueshift', 'Duality', 'Elliott']
                      }
 EQ_HEDGE_DATA_FP = RETURNS_DATA_FP+'eq_hedge_returns.xlsx'
 EQ_HEDGE_STRAT_DICT = {'99%/90% Put Spread':0.0, 'Down Var':1.0, 'Vortex':0.0, 'VOLA':1.25,'Dynamic Put Spread':1.0,
@@ -33,16 +32,16 @@ def read_ret_data(fp, sheet_name):
     ret_data = pd.read_excel(fp, sheet_name, index_col=0)
     return dm.get_real_cols(ret_data)
 
-class bmkHandler():
+class mktHandler():
     def __init__(self, equity_bmk = 'M1WD',include_fi = True, all_data=False):
         self.equity_bmk = equity_bmk
         self.include_fi = include_fi
         if include_fi:
             self.fi_bmk = 'FI Benchmark'
         self.all_data = all_data
-        self.bmk_returns = self.get_bmk_returns()
+        self.mkt_returns = self.get_mkt_returns()
         
-    def get_bmk_returns(self):
+    def get_mkt_returns(self):
         returns_dict = {}
         for freq in FREQ_LIST:
             freq_string = dm.switch_freq_string(freq)
@@ -62,32 +61,41 @@ class bmkHandler():
         
         return returns_dict
         
-class liqAltsBmkHandler(bmkHandler):
+class liqAltsBmkHandler(mktHandler):
     def __init__(self, equity_bmk = 'M1WD',include_fi = True, all_data=False):
-        bmkHandler.__init__(self, equity_bmk, include_fi)
+        mktHandler.__init__(self, equity_bmk, include_fi)
         self.equity_bmk = equity_bmk
         self.include_fi = include_fi
         self.all_data = all_data
-        self.returns = self.get_returns()
+        self.hf_returns = self.get_returns(False)
+        self.bmk_returns = self.get_returns()
         
-    def get_returns(self):
+    def get_returns(self, bmk_data = True):
         returns_dict = {}
         for freq in FREQ_LIST:
             freq_string = dm.switch_freq_string(freq)
-            temp_ret = read_ret_data(LIQ_ALTS_BMK_DATA_FP, freq_string)
-            if not self.all_data:
-                temp_ret['Liquid Alts Bmk'] = 0.5*temp_ret['HFRX Macro/CTA'] + 0.3*temp_ret['HFRX Absolute Return'] + 0.2*temp_ret['SG Trend']
+            if bmk_data:
+                temp_ret = read_ret_data(LIQ_ALTS_BMK_DATA_FP, freq_string)
+                if not self.all_data:
+                    temp_ret['Liquid Alts Bmk'] = 0.5*temp_ret['HFRX Macro/CTA'] + 0.3*temp_ret['HFRX Absolute Return'] + 0.2*temp_ret['SG Trend']
+            else:
+                temp_ret = read_ret_data(HF_BMK_DATA_FP, freq_string)
             returns_dict[freq_string] = temp_ret.copy()
         return returns_dict
+    
 
-class liqAltsPortHandler(bmkHandler):
+class liqAltsPortHandler(liqAltsBmkHandler):
     def __init__(self, equity_bmk = 'M1WD',include_fi = True):
-        bmkHandler.__init__(self, equity_bmk, include_fi)
+        liqAltsBmkHandler.__init__(self, equity_bmk, include_fi)
         self.equity_bmk = equity_bmk
         self.include_fi = include_fi
         self.sub_ports = self.get_sub_port_data()
         self.returns = self.get_full_port_data()
         self.mvs = self.get_full_port_data(False)
+        #TODO: add get_bmk_returns
+        self.hf_returns = self.hf_returns['Monthly']
+        self.bmk_returns = self.bmk_returns['Monthly']
+        self.mkt_returns = self.mkt_returns['Monthly']
         
     def get_sub_port_data(self):
         #pull all returns and mvs
@@ -102,19 +110,21 @@ class liqAltsPortHandler(bmkHandler):
             temp_dict = {}
             temp_ret = liq_alts_ret[LIQ_ALTS_MGR_DICT[key]].copy()
             temp_mv = liq_alts_mv[LIQ_ALTS_MGR_DICT[key]].copy()
-            liq_alts_dict[key] = {'returns': temp_ret, 'mv': temp_mv}
             temp_dict = dm.get_agg_data(temp_ret, temp_mv, key)
-            # if key == 'Trend Following':
-            #     temp_dict = {'returns': liq_alts_ret[[key]], 'mv':liq_alts_mv[[key]]}
+            if key == 'Trend Following':
+                temp_dict = {'returns': liq_alts_ret[[key]], 'mv':liq_alts_mv[[key]]}
+            temp_ret =  dm.merge_data_frames(temp_ret, temp_dict['returns'], False)
+            temp_mv = dm.merge_data_frames(temp_mv, temp_dict['mv'], False)
+            liq_alts_dict[key] = {'returns': temp_ret, 'mv': temp_mv}
             total_ret = dm.merge_data_frames(total_ret, temp_dict['returns'], False)
             total_mv = dm.merge_data_frames(total_mv, temp_dict['mv'], False)
+        total_dict = dm.get_agg_data(total_ret, total_mv, 'Total Liquid Alts')
+        total_ret = dm.merge_data_frames(total_ret, total_dict['returns'], False)
+        total_mv = dm.merge_data_frames(total_mv, total_dict['mv'], False)
         liq_alts_dict['Total Liquid Alts'] = {'returns': total_ret, 'mv':total_mv}
+        
         return liq_alts_dict
     
-    # def get_total(self, total_ret, total_mv):
-    #     total_dict = dm.get_agg_data(total_ret, total_mv, 'Total Liquid Alts')
-    #     return {'returns': total_dict['returns'][['Total Liquid Alts']],
-    #             'mv':total_dict['mv'][['Total Liquid Alts']]}
     
     def get_full_port_data(self, return_data = True):
         data = 'returns' if return_data else 'mv'
@@ -142,9 +152,9 @@ class liqAltsPortHandler(bmkHandler):
         return mgr_list
 
 #TODO: add weighted strats logic
-class eqHedgeHandler(bmkHandler):
+class eqHedgeHandler(mktHandler):
     def __init__(self, equity_bmk = 'M1WD',include_fi = True, all_data=False, strat_drop_list=[]):
-        bmkHandler.__init__(self, equity_bmk, include_fi)
+        mktHandler.__init__(self, equity_bmk, include_fi)
         self.equity_bmk = equity_bmk
         self.include_fi = include_fi
         self.all_data = all_data
