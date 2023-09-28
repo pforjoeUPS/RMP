2c1b236f40857a17c67e5d97ff2c0e122e04799d
samspark14 <106697943+samspark14@users.noreply.github.com>
2023-09-28 10:44:02 -0400

    Update strat_report_script.py

--- EquityHedging/scripts/strat_report_script.py
+++ EquityHedging/scripts/strat_report_script.py
@@ -12,8 +12,8 @@ from EquityHedging.reporting import formatter as fmt
 import pandas as pd
 
 equity_bmk = 'SPTR'
-strat_drop_list = ['SPTR', '99%/90% Put Spread','Down Var', 'Vortex', 'Dynamic Put Spread',
-                   'Def Var','Corr Hedge', 'VOLA', 'GW Dispersion']
+strat_drop_list = ['99%/90% Put Spread','Down Var', 'Vortex', 'Dynamic Put Spread',
+                   'Def Var','Corr Hedge', 'VOLA', 'GW Dispersion', 'VRR 2', 'VRR Trend', 'Commodity Basket', 'ESPRSO', 'EVolCon']
 include_fi = False
 
 #create returns data dictionary for equity benchmark
@@ -36,21 +36,15 @@ returns= dm.get_equity_hedge_returns(equity_bmk, include_fi, strat_drop_list, on
 
 
 #Get new strategy returns
-filename = 'Nomura_Rate_Vol.xlsm'
+filename = 'UBS_defensive_skew_data.xlsx'
 sheet_name = 'Sheet1'
 new_strat = pd.read_excel(dm.NEW_DATA + filename,
                                            sheet_name = 'Sheet1', index_col=0)
 new_strat_dict = dm.get_data_dict(new_strat)
 
-filename = 'UBS Def Rates Overly.xlsx'
-sheet_name = 'Sheet1'
-new_strat = pd.read_excel(dm.NEW_DATA + filename,
-                                           sheet_name = 'Sheet1', index_col=0)
-new_strat_dict1 = dm.get_data_dict(new_strat)
-
 #merge dictionaries
-full_data_dict = dm.merge_dicts_list([returns, new_strat_dict, new_strat_dict1])
+full_data_dict = dm.merge_dicts_list([returns, new_strat_dict])
 
-strat_report_name = 'Rates Vol Strat - VRR,Nomura,UBS - Analysis'
+strat_report_name = 'UBS_Defensive_Skew_Report_wo_BMK'
 selloffs = True
-rp.generate_strat_report(strat_report_name, full_data_dict, selloffs = True)
+rp.generate_strat_report(strat_report_name, new_strat_dict, selloffs = True)
