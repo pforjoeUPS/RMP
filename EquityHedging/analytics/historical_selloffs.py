04a0283b6f9659d5c83bad728f99ab6eb37e055a
pforjoeUPS <pforjoe@ups.com>
2023-12-21 22:40:19 -0500

    Update historical_selloffs.py

--- EquityHedging/analytics/historical_selloffs.py
+++ EquityHedging/analytics/historical_selloffs.py
@@ -8,6 +8,11 @@ Created on Wed Nov 18 17:15:17 2020
 from ..datamanager import data_manager as dm
 from .import util
 
+
+SPTR_MAXDD = {'period':'SPTR Max DD',
+              'start': '2008-01-01', 
+              'end':'2009-03-09'}
+
 GFC = {'period':'Financial Crisis', 
        'start': '2008-05-19', 
        'end':'2009-03-09'}
@@ -21,8 +26,8 @@ US_DEBT = {'period':'US Debt Downgrade',
            'end':'2011-10-03'}
 
 CHINA_WEAKNESS = {'period':'China weakness concerns', 
-              'start': '2015-08-10', 
-              'end':'2015-08-25'}
+                  'start': '2015-08-10', 
+                  'end':'2015-08-25'}
 
 OIL_DROP = {'period':'Drop in oil prices, China Instability', 
             'start': '2015-12-29', 
@@ -37,11 +42,10 @@ Q4_2018 = {'period':'Q4 2018',
            'end':'2018-12-24'}
 
 COVID_19 = {'period':'Coronavirus Pandemic', 
-          'start': '2020-02-19', 
-          'end':'2020-03-23'}
+            'start': '2020-02-19', 
+            'end':'2020-03-23'}
 
-
-EVENTS = [GFC, EURO_DEBT, US_DEBT, CHINA_WEAKNESS,
+EVENTS = [SPTR_MAXDD, GFC, EURO_DEBT, US_DEBT, CHINA_WEAKNESS,
           OIL_DROP, FEB_2018, Q4_2018, COVID_19]
 
 
@@ -131,40 +135,27 @@ def get_hist_sim_table(df_returns, notional_weights=[], weighted=False):
     hist_dict['Start'] = separate_events(EVENTS)['Start']
     hist_dict['End'] = separate_events(EVENTS)['End']
     
-    #remove_dict and period_list for data to removed from hist_dict
-    remove_dict = {}
-    period_list = []
-        
-        
     #loop through columns to create dictionary
-    for col in df_index.columns:
+    for col in df_index:
+        print(col)
         strat_df = dm.remove_na(df_index, col)
         
         #temp_col to store strategy selloff data
         temp_col = []
-        
+        temp_dict = {}
         for event in EVENTS:
             start = event['start']
             end = event['end']
+            period = event['period']
             try:
                 event_return = compute_event_ret(strat_df, col, start, end)
                 temp_col.append(event_return)
+                temp_dict[period] = event_return
             except KeyError:
-                #check if event is in temp_list to prevent repetitve printouts
-                if event['period'] not in period_list:
-                    print('Skipping {} selloff event'.format(event['period']))
-                    period_list.append(event['period'])
-                #add period to dictionary to remove after looping through columns
-                remove_dict[event['period']]= (start,end)
+                print('Skipping {} selloff event for {}'.format(period, col))
+                temp_col.append(float("nan"))
                 pass
         hist_dict[col] = temp_col
-    
-    #remove event data from hist_dict if event was skipped
-    if remove_dict:
-        for key in remove_dict:
-            hist_dict['Start'].remove(remove_dict[key][0])
-            hist_dict['End'].remove(remove_dict[key][1])
-            periods.remove(key)
 
     #convert dictionary to dataframe
     df_hist = util.convert_dict_to_df(hist_dict, periods)
