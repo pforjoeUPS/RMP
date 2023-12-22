0566b61eb3ea9ecf3f1ed257af5961225d4d619f
pforjoeUPS <pforjoe@ups.com>
2023-12-21 22:39:17 -0500

    Update corr_stats.py

--- EquityHedging/analytics/corr_stats.py
+++ EquityHedging/analytics/corr_stats.py
@@ -7,10 +7,11 @@ Created on Tue Oct  1 17:59:28 2019
 
 import pandas as pd
 import numpy as np
-from ..datamanager.data_manager import get_min_max_dates
+from ..datamanager import data_manager as dm
 from .import util
 
-def get_corr_analysis(df_returns, notional_weights=[], include_fi=False, weighted=False):
+
+def get_corr_analysis(df_returns, notional_weights=[], include_fi=False, weighted=False, include_bmk=False):
     """
     Returns a dict of correlation matrices: 
         full correlation, when equity up, when equity down
@@ -46,13 +47,14 @@ def get_corr_analysis(df_returns, notional_weights=[], include_fi=False, weighte
     equity_up = (strategy_returns[strategy_returns[equity_id] > 0])
     equity_down = (strategy_returns[strategy_returns[equity_id] < 0])
 
-    dates = get_min_max_dates(strategy_returns)
+    dates = dm.get_min_max_dates(strategy_returns)
     data_range = str(dates['start']).split()[0] + ' to ' + str(dates['end']).split()[0]
     
     corr_dict = {
         "full": [strategy_returns.corr(), get_title_string(strategy_returns, data_range,equity_id)],
         "equity_up": [equity_up.corr(), get_title_string(equity_up, data_range,equity_id,'index_up')],
-        "equity_down": [equity_down.corr(), get_title_string(equity_up, data_range,equity_id,'index_down')]
+        "equity_down": [equity_down.corr(), get_title_string(equity_up, data_range,equity_id,'index_down')],
+        "equity_up_down": [get_up_lwr_corr(equity_up, equity_down), get_title_string(strategy_returns, data_range,equity_id)]
         }
     
     if include_fi:
@@ -61,17 +63,93 @@ def get_corr_analysis(df_returns, notional_weights=[], include_fi=False, weighte
         fi_down = (strategy_returns[strategy_returns[fi_id] < 0])
         corr_dict["fi_up"] = [fi_up.corr(), get_title_string(fi_up, data_range,fi_id,'index_up')]
         corr_dict["fi_down"] = [fi_down.corr(), get_title_string(fi_up, data_range,fi_id,'index_down')]
+        corr_dict["fi_up_down"]= [get_up_lwr_corr(fi_up, fi_down), get_title_string(strategy_returns, data_range,fi_id)]
+        
+    
+    if include_bmk:
+        pass
+        # bmk_id = col_list[2]
+        # bmk_up = (strategy_returns[strategy_returns[bmk_id] > 0])
+        # bmk_down = (strategy_returns[strategy_returns[bmk_id] < 0])
+        # corr_dict["bmk_up"] = [bmk_up.corr(), get_title_string(bmk_up, data_range,bmk_id,'index_up')]
+        # corr_dict["bmk_down"] = [bmk_down.corr(), get_title_string(bmk_up, data_range,bmk_id,'index_down')]
 
     return corr_dict
 
+def get_corr_analysis1(df_returns, notional_weights=[], include_eq=False,include_fi=False, weighted=False):
+    """
+    Returns a dict of correlation matrices: 
+        full correlation, when equity up, when equity down
 
-def get_title_string(returns_df,data_range,eq_fi_id,scenario='full'):
-    title = 'Correlation of {} Historical Observations ({})'.format(str(len(returns_df)), data_range)
+    Parameters
+    ----------
+    df_returns : dataframe
+    notional_weights : list, optional
+        The default is [].
+    include_fi : boolean, optional
+        The default is False.
+    weighted : boolean, optional
+        The default is False.
+
+    Returns
+    -------
+    corr_dict : dictionary
+        {key: string, value: list[dataframe, string]}
+
+    """
+
+    strategy_returns = df_returns.copy()
+    
+    #compute weighted hedges returns
+    if weighted:
+        strategy_returns = util.get_weighted_hedges(strategy_returns, notional_weights, include_fi)
+
+    #get equity value (e.g. 'SPTR', 'M1WD', 'SX5T')
+    col_list = list(strategy_returns.columns)
+    
+    #get returns when equity > 0 and equity < 0
+    # equity_up = (strategy_returns[strategy_returns[equity_id] > 0])
+    # equity_down = (strategy_returns[strategy_returns[equity_id] < 0])
+
+    corr_dict = {"full": [strategy_returns.corr(), 
+                          f'Correlation of {len(strategy_returns)} Historical Observations ({get_data_range(strategy_returns)})']}
+    if include_eq:
+        equity_id = col_list[0]
+        corr_dict["Equity"] = get_conditional_corr(strategy_returns, equity_id)
+    
+    if include_fi:
+        fi_id = col_list[1]
+        corr_dict["Fixed Income"]= get_conditional_corr(strategy_returns, fi_id)
+                                   
+    
+    return corr_dict
+
+def get_data_range(df_returns):
+    dates = dm.get_min_max_dates(df_returns)
+    return str(dates['start']).split()[0] + ' to ' + str(dates['end']).split()[0]
+
+def get_conditional_corr(df_returns, strat_id):
+    strat_up = (df_returns[df_returns[strat_id] > 0])
+    strat_down = (df_returns[df_returns[strat_id] < 0])
+    return [get_up_lwr_corr(strat_up, strat_down),  f'Conditional correlation where {strat_id} > 0 (upper) and < 0 (lower)']
+
+def get_up_lwr_corr(up_df, lwr_df):
+    up_array = np.triu(up_df.corr().values)
+    lwr_array = np.tril(lwr_df.corr().values)
+    temp_array = up_array + lwr_array
+    for i in range(0,len(temp_array)):
+        temp_array[i][i] = 1
+        
+    return pd.DataFrame(temp_array, index=lwr_df.columns, columns=up_df.columns)
+
+def get_title_string(returns_df,strat_id,scenario='full'):
+    
+    title = 'Correlation of {} Historical Observations ({}) '.format(str(len(returns_df)), get_data_range(returns_df))
     
     if scenario == 'index_up':
-        return title + 'where ' + eq_fi_id + ' > 0'
+        return title + 'where ' + strat_id + ' > 0'
     elif scenario == 'index_down':
-        return title + 'where ' + eq_fi_id + ' < 0'
+        return title + 'where ' + strat_id + ' < 0'
     else:
         return title
 #TODO: make flexible to compute corrs w/o weighted strats/hedges
@@ -137,3 +215,60 @@ def get_corr_rank_data(df_returns,buckets, notional_weights=[],include_fi=False)
         corr_pack[key] = [list_df[rank].corr(), value]
     
     return corr_pack
+
+
+def get_rolling_corr_data(df_returns,window=36):
+    """
+    Get a dictionary contaiing rolling correlations of each strategy in df_returns vs the other strategies
+
+    Parameters
+    ----------
+    df_returns : TYPE
+        DESCRIPTION.
+    window : int
+        rolling window.
+
+    Returns
+    -------
+    rolling_corr_dict : Dictionary
+        DESCRIPTION.
+
+    """
+    rolling_corr_dict = {}
+    
+    for strat_1 in df_returns:
+        #get list w/o strat_1
+        temp_strat_list = list(df_returns.columns).copy()
+        temp_strat_list.remove(strat_1)
+        
+        #create a df of rolling correlations to strat_1
+        rolling_corr_temp = get_rolling_corr(df_returns[strat_1],df_returns[strat_1], window)
+        for strat_2 in temp_strat_list:
+            rolling_corr_temp = dm.merge_data_frames(rolling_corr_temp, get_rolling_corr(df_returns[strat_1],df_returns[strat_2], window),False)
+        rolling_corr_temp.drop([strat_1], axis=1, inplace=True)
+        rolling_corr_dict[strat_1] = rolling_corr_temp
+    return rolling_corr_dict
+    
+def get_rolling_corr(ret_series_1, ret_series_2, window=36):
+    """
+    Get rolling correlation between 2 return series
+
+    Parameters
+    ----------
+    ret_series_1 : Series
+        returns.
+    ret_series_2 : Series
+        returns.
+    window : int
+        rolling window.
+
+    Returns
+    -------
+    rolling_corr : Dataframe
+        correlation Dataframe.
+
+    """
+    rolling_corr = pd.DataFrame(ret_series_1.rolling(window).corr(ret_series_2), columns=[ret_series_2.name])
+    rolling_corr.dropna(inplace=True)
+    return rolling_corr
+
