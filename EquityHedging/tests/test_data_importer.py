f7d46c3550924c149c29f5569070e2116ad03937
Devang0903 <devangajmera09@gmail.com>
2024-02-05 15:41:42 -0500

    Added test_data_importer_import_data

--- EquityHedging/tests/test_data_importer.py
+++ EquityHedging/tests/test_data_importer.py
@@ -101,6 +101,31 @@ class TestDataImporterInitialization(BaseDataImporterTest):
         )
 
 class TestDataImporterMethods(BaseDataImporterTest):
+
+    @patch('EquityHedging.datamanager.data_importer.read_excel_data')
+    @patch('EquityHedging.datamanager.data_importer.dm.drop_nas')
+    @patch('EquityHedging.datamanager.data_importer.dm.rename_columns')
+    def test_data_importer_import_data(self, mock_rename_columns, mock_drop_nas, mock_read_excel_data):
+        mock_data = pd.DataFrame({'col1': [1, 2, 3], 'col2': ['a', 'b', 'c']})
+        mock_read_excel_data.return_value = mock_data
+        mock_drop_nas.return_value = mock_data
+        mock_rename_columns.return_value = mock_data
+
+        data_importer = DataImporter('test_filepath.xlsx')
+        result = data_importer.import_data()
+
+        mock_read_excel_data.assert_called_with('test_filepath.xlsx', 0, index_col=0, skip_rows=[])
+        mock_drop_nas.assert_called_with(mock_data)
+
+        # Check if rename_columns is called
+        if data_importer.col_dict:
+            mock_rename_columns.assert_called_with(mock_data, col_dict={})
+        else:
+            mock_rename_columns.assert_not_called()
+
+        self.assertTrue(result.equals(mock_data))
+
+
     @patch('EquityHedging.datamanager.data_importer.read_excel_data')
     def test_bbg_data_importer_get_col_dict(self, mock_read_excel_data):
         mock_keys_data = pd.DataFrame({'Index': ['A', 'B'], 'Description': ['DescA', 'DescB']})
