1b9cf0e75b042ab8645aaaad3ea9d41e1cdac8c8
pforjoeUPS <pforjoe@ups.com>
2024-02-26 23:25:32 -0500

    Update script_new.py

--- EquityHedging/scripts/script_new.py
+++ EquityHedging/scripts/script_new.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan 24 2023
-
-@author: Powis Forjoe, Zach Wells and Maddie Choi
-"""
 # import os
 #
 # os.chdir('..\..')
@@ -23,9 +17,7 @@ weighted = True
 strat_drop_list = []
 new_strat = False
 
-eq_hedge_dh = dh.EQHedgeDataHandler(eq_bmk=eq_bmk, eq_mv=11.0,
-                                    include_fi=include_fi, fi_mv=20.0,
-                                    strat_drop_list=[])
+eq_hedge_dh = dh.EQHedgeDataHandler(eq_bmk=eq_bmk, eq_mv=11.0, include_fi=include_fi, fi_mv=20.0, strat_drop_list=[])
 
 # Add new strat
 new_strat = True
@@ -55,49 +47,36 @@ check_corr = False
 if check_corr:
     corr_freq_list = ['Daily', 'Weekly', 'Monthly']
     eq_hedge_analytics.get_corr_stats_dict(corr_freq_list)
-    corr_dict = EquityHedging.datamanager.data_manager_new.copy_data(eq_hedge_analytics.corr_stats_dict)
+    corr_dict = dm.copy_data(eq_hedge_analytics.corr_stats_dict)
 
 # compute analytics
-# import time
-# start = time.time()
 check_analysis = True
 if check_analysis:
     analytics_freq_list = ['Weekly', 'Monthly']
     eq_hedge_analytics.get_returns_stats_dict(analytics_freq_list)
     eq_hedge_analytics.get_hedge_metrics_dict(analytics_freq_list)
     analytics_dict = {
-        'return_stats': EquityHedging.datamanager.data_manager_new.copy_data(eq_hedge_analytics.returns_stats_dict),
-        'hedge_metrics': EquityHedging.datamanager.data_manager_new.copy_data(eq_hedge_analytics.hedge_metrics_dict)
+        'return_stats':dm.copy_data(eq_hedge_analytics.returns_stats_dict),
+        'hedge_metrics': dm.copy_data(eq_hedge_analytics.hedge_metrics_dict)
     }
-
-# end = time.time()
-# print(end - start)
 
 # compute historical selloffs
 check_hs = False
 if check_hs:
     eq_hedge_analytics.get_hist_selloff()
-    hist_df = EquityHedging.datamanager.data_manager_new.copy_data(eq_hedge_analytics.historical_selloff_data)
+    hist_df = dm.copy_data(eq_hedge_analytics.historical_selloff_data)
 
 # get quantile dataframe
 check_quantile = False
 if check_quantile:
     eq_hedge_analytics.get_quantile_stats()
-    quantile_dict = EquityHedging.datamanager.data_manager_new.copy_data(eq_hedge_analytics.quantile_stats_data)
-
-# #get annual dollar returns dataframe
-# check_ann = False
-# if check_ann:
-#     annual_dollar_returns = summary.get_annual_dollar_returns(returns, notional_weights)
+    quantile_dict = dm.copy_data(eq_hedge_analytics.quantile_stats_data)
 
 # run report
 report_name = 'equity_hedge_analysis_testNew'
 selloffs = True
 rp.EquityHedgeReport(report_name=report_name, data_handler=eq_hedge_dh, weighted=weighted,
                      new_strat=new_strat, selloffs=selloffs).run_report()
-
-# end = time.time()
-# print(end - start)
 
 hs_report = 'historical_selloff_test'
 rp.HistSelloffReport(report_name=hs_report, data_handler=eq_hedge_dh, weighted=weighted,
